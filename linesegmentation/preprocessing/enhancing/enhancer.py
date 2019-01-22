14bbf77cfcf3d9646ee259da92b01c2a5754fc64
Alexander Hartelt <hartelt.alexander@gmail.com>
2019-01-22 19:35:54 +0100

    Changes to low pass filter + debug messages

--- linesegmentation/preprocessing/enhancing/enhancer.py
+++ linesegmentation/preprocessing/enhancing/enhancer.py
@@ -2,7 +2,29 @@ from scipy.signal import convolve2d
 import numpy as np
 import cv2
 
+
 def enhance(image):
     image = cv2.bilateralFilter((image * 255).astype(np.uint8), 5, 75, 75)
     image = (1 - np.clip(convolve2d(1 - image, np.full((1, 10), 0.2)), 0, 255)) / 255
     return image
+
+
+if __name__ == "__main__":
+    from PIL import Image
+    from matplotlib import pyplot as plt
+    import os
+    from linesegmentation.preprocessing.binarization.ocropus_binarizer import binarize
+
+    project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    page_path = os.path.join(project_dir, 'demo/images/Graduel_de_leglise_de_Nevers-509.nrm.png')
+    gray_image = np.array(Image.open(page_path)) / 255
+    enhanced_image = enhance(gray_image)
+    f, ax = plt.subplots(1, 2, True, True)
+    ax[0].imshow(gray_image, cmap='gray')
+    ax[1].imshow(enhanced_image, cmap='gray')
+    plt.show()
+
+    f, ax = plt.subplots(1, 2, True, True)
+    ax[0].imshow(binarize(gray_image), cmap='gray')
+    ax[1].imshow(binarize(enhanced_image), cmap='gray')
+    plt.show()
--- linesegmentation/util/image_util.py
+++ linesegmentation/util/image_util.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image, ImageStat
-
+import math
 
 def normalize_raw_image(raw):
     image = raw.astype(np.float32) - np.amin(raw)
@@ -13,7 +13,7 @@ def smooth_array(values, smoothing):
     for i in range(len(values)):
         current_value = values[i]
         value += (current_value - value) / smoothing
-        values[i] = int(value)
+        values[i] = int(math.floor(value + 0.5))
     return values
 
 
@@ -39,3 +39,32 @@ def detect_color_image(file, thumb_size=40, MSE_cutoff=22, adjust_color_bias=Tru
         return "binary"
     else:
        return "other"
+
+if __name__ == "__main__":
+    import os
+    from linesegmentation.detection.lineDetector import LineDetectionSettings
+    from linesegmentation.detection.lineDetection import LineDetection
+    from PIL import Image
+    from matplotlib import pyplot as plt
+
+    setting_predictor = LineDetectionSettings(debug=False, post_process=True)
+    line_detector = LineDetection(setting_predictor)
+    project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    page_path = os.path.join(project_dir, 'demo/images/Graduel_de_leglise_de_Nevers-509.nrm.png')
+    for _pred in line_detector.detect([page_path]):
+        im = plt.imread(page_path)
+        f, ax = plt.subplots(1, 2, True, True)
+        ax[0].imshow(im, cmap='gray')
+        cmap = plt.get_cmap('jet')
+        colors = cmap(np.linspace(0, 1.0, len(_pred)))
+        for system, color in zip(_pred, colors):
+            for staff in system:
+                y, x = zip(*staff)
+                ax[0].plot(x, y, color=color)
+        ax[1].imshow(im, cmap='gray')
+        for system, color in zip(_pred, colors):
+            for staff in system:
+                y, x = zip(*staff)
+                y = smooth_array(list(y), 1.3)
+                ax[1].plot(x, y, color=color)
+        plt.show()
