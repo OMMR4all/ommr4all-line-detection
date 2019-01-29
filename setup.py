5504613379a4db48e4afba8bf5d8045d38bb8ba2
Christoph Wick <wick.chr.info@gmail.com>
2019-01-29 10:32:21 +0100

    moved tensorflow to extras_requires of setup script

--- setup.py
+++ setup.py
@@ -18,6 +18,10 @@ setup(
         ],
     },
     install_requires=open("requirements.txt").read().split(),
+    extras_require={
+        'tf_cpu': ['tensorflow>=1.6.0'],
+        'tf_gpu': ['tensorflow-gpu>=1.6.0'],
+    },
     keywords=['OMR', 'staff line detection', 'pixel classifier'],
     data_files=[('', ["requirements.txt"])],
 )
